9a8c781f1548239285fd6d4150b3a13e2db0641b
haotengteng <haot_mail@163.com>
2016-03-27 16:02:56 +0800

    update

--- zuoan/src/main/java/com/zuoan/filter/LoginCheckFilter.java
+++ zuoan/src/main/java/com/zuoan/filter/LoginCheckFilter.java
@@ -3,6 +3,7 @@ package com.zuoan.filter;
 import com.zuoan.ApiProvider.ApiProvider;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,11 +12,12 @@ import java.io.IOException;
 /**
  * Created by haotengteng on 2016/3/27.
  */
+@WebFilter(filterName = "LoginCheckFilter", urlPatterns = "/*")
 public class LoginCheckFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        System.out.println("初始化过滤器");
     }
 
     @Override
